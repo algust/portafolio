7562145316a8f0a91f9777764b421ebf76d3bc9c
Gustavo Taboada <slogan1918@gmail.com>
2020-09-06 18:02:53 -0500

    Updates

---
+++ precache-manifest.66a8f725a5501d8bebaec6e5b76c90b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7dadebcd64884c34dfd8f5178402bf",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "bdcf42940f6d299d1215",
+    "url": "/portafolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c8550a42f4e035516de8",
+    "url": "/portafolio/static/js/2.a9857d7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.a9857d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdcf42940f6d299d1215",
+    "url": "/portafolio/static/js/main.716deed9.chunk.js"
+  },
+  {
+    "revision": "fd9719b537e1bda7b1e1",
+    "url": "/portafolio/static/js/runtime-main.18288a4a.js"
+  }
+]);
